b9021178406803f938c5b2049cbb18316467ea65
ray <chunlei_uis@hotmail.com>
2022-08-28 09:26:51 +0500

    add access-db dag

---
+++ dags/access_metadb/airflow_db.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime, timedelta
+
+
+def get_dag_ids():
+    postgres_hook = PostgresHook(postgres_conn_id="airflow_db")
+    records = postgres_hook.get_records(sql="select dag_id from dag")
+    print(records)
+
+
+with DAG(
+    "connect_dag",
+    start_date=datetime(2019, 1, 1),
+    max_active_runs=1,
+    schedule_interval=None,
+    # catchup=False # enable if you don't want historical dag runs to run
+) as dag:
+
+    t1 = PythonOperator(
+        task_id="get_dag_nums",
+        python_callable=get_dag_ids,
+    )
